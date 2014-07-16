d160f324e511608111905bfd3cd755056b8a11c1
Henrik Östman <trycoon@gmail.com>
2014-07-16 11:33:43 +0200

    Uppgraderat till Visual Studio 2013 och .net 4.5.2. Tagit bort Combres och ersatt med Microsoft Web Optimization Framework (funkade bättre med Handlebars). Lagt in Handlebars.js, funkar dock inte 100% ännu.

---
+++ FiberKartan/Handlebars/BundleConfig.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Web.Optimization;
+using Controllers.Transform;
+
+namespace FiberKartan
+{
+    public class BundleConfig
+    {
+        public static void RegisterHandlBarBundles(BundleCollection bundles, string path)
+        {
+            HandleBarBundleTransform transform = new HandleBarBundleTransform();
+            transform.jsPath = path;
+            bundles.Add(new Bundle("~/inc/views", transform).IncludeDirectory("~/Handlebars/views", "*.hbs", true));
+            #if !DEBUG
+                BundleTable.EnableOptimizations = true;
+            #endif
+        }
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // http://www.codeproject.com/Articles/748849/ASP-NET-Web-Optimization-Framework
+
+            bundles.Add(new StyleBundle("~/inc/adminCss").Include(
+                 "~/inc/css/base.css",
+                "~/inc/css/jquery-ui.min.css",
+                "~/inc/css/map.css",
+                "~/inc/css/jquery.contextMenu.css"
+                ).ForceOrdered());
+
+            bundles.Add(new StyleBundle("~/inc/userCss").Include(
+                "~/inc/css/base.css",
+                "~/inc/css/jquery-ui.min.css",
+                "~/inc/css/map.css"
+               ).ForceOrdered());
+
+            bundles.Add(new ScriptBundle("~/inc/adminJs").Include(
+               "~/inc/js/jquery.min.js",
+               "~/inc/js/jquery-ui.min.js",
+               "~/inc/js/jquery.ui.touch-punch.min.js",
+               "~/inc/js/base.js",
+                "~/inc/js/jquery.contextMenu.js",
+                "~/inc/js/mapAdmin.js",
+                "~/inc/js/markerwithlabel.js",
+                "~/inc/js/jquery.fileupload.js",
+                "~/inc/js/jquery.iframe-transport.js",
+                "~/inc/js/jquery.cookie.js"
+                ).ForceOrdered());
+
+            bundles.Add(new ScriptBundle("~/inc/userJs").Include(
+               "~/inc/js/jquery.min.js",
+               "~/inc/js/jquery-ui.min.js",
+               "~/inc/js/jquery.ui.touch-punch.min.js",
+               "~/inc/js/base.js",
+                "~/inc/js/map.js"
+                ).ForceOrdered());
+
+            bundles.Add(new ScriptBundle("~/inc/regionJs").Include(
+               "~/inc/js/jquery.min.js",
+               "~/inc/js/jquery-ui.min.js",
+               "~/inc/js/jquery.ui.touch-punch.min.js",
+               "~/inc/js/base.js",
+                "~/inc/js/totalMap.js"
+                ).ForceOrdered());
+
+            bundles.Add(new ScriptBundle("~/inc/incidentReportJs").Include(
+               "~/inc/js/jquery.min.js",
+               "~/inc/js/jquery-ui.min.js",
+               "~/inc/js/jquery.ui.touch-punch.min.js",
+               "~/inc/js/base.js",
+                "~/inc/js/incidentReportMap.js"
+                ).ForceOrdered());
+
+            #if !DEBUG
+                BundleTable.EnableOptimizations = true;
+            #endif
+        }
+    }
+
+    internal class AsIsBundleOrderer : IBundleOrderer
+    {
+        public virtual IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+
+    internal static class BundleExtensions
+    {
+        public static Bundle ForceOrdered(this Bundle sb)
+        {
+            sb.Orderer = new AsIsBundleOrderer();
+            return sb;
+        }
+    }
+}
---
+++ FiberKartan/Handlebars/HandleBarBundleTransform.cs
@@ -0,0 +1,131 @@
+﻿using System.Web;
+using Jurassic;
+using System.IO;
+using System.Web.Optimization;
+using System;
+using Microsoft.Ajax.Utilities;
+namespace Controllers.Transform
+{
+    public class HandleBarBundleTransform : IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            string content = "Handlebars.templates = {};";
+            foreach (var assetFile in response.Files)
+            {
+                var result = GetViewTuple(context, assetFile);
+                content += PrecompileHandlebarsTemplate(result.Item1, result.Item2);
+            }
+
+            if (minifyTemplates)
+            {
+                var minifier = new Minifier();
+                var c = minifier.MinifyJavaScript(content);
+                if (minifier.ErrorList.Count <= 0)
+                {
+                    content = c;
+                }
+            }
+            content += ";";
+            content += @"Handlebars.partials = Handlebars.templates;
+                          templates = Handlebars.templates;";
+            response.ContentType = "text/javascript";
+            response.Cacheability = HttpCacheability.Public;
+            response.Content = content;
+        }
+
+        public string PrecompileHandlebarsTemplate(string name, string template)
+        {
+            var engine = new ScriptEngine();
+
+            engine.ExecuteFile(jsPath);
+            engine.Execute(@"var precompile = Handlebars.precompile;");
+            return string.Format("Handlebars.templates[\"{0}\"] = Handlebars.template({1});",
+                name, engine.CallGlobalFunction("precompile", template).ToString());
+        }
+
+        public Tuple<String, String> GetViewTuple(BundleContext context, BundleFile assetFile)
+        {
+            string template;
+            string templateName;
+
+            var virtual_root = assetFile.IncludedVirtualPath.Substring(0, assetFile.IncludedVirtualPath.IndexOf("\\"));
+
+            var virtual_file_path = string.Format("~{0}", assetFile.VirtualFile.VirtualPath);
+
+            var application_path = HttpContext.Current != null ? HttpContext.Current.Request.ApplicationPath : "/";
+
+            var app_less_virtual_file_path = virtual_file_path.Replace("~" + application_path, String.Empty).TrimStart('/');
+
+            virtual_file_path = string.Format("~/{0}", app_less_virtual_file_path).Replace(virtual_root + "/", string.Empty);
+
+            var file_extension = virtual_file_path.Substring(virtual_file_path.LastIndexOf("."));
+
+            var template_directory_name = string.Empty;
+
+            var template_file_name = virtual_file_path
+                                    .Replace("~", string.Empty)
+                                    .Replace(file_extension, string.Empty);
+
+            if (template_file_name.IndexOf(this.resourceNameSeparator) >= 0)
+            {
+                var segments = template_file_name.Split(this.resourceNameSeparator[0]);
+                if (1 < segments.Length)
+                {
+                    var template_file_name_temp = string.Empty;
+                    if (2 == segments.Length)
+                    {
+
+                        template_directory_name = segments[0];
+                        template_file_name_temp = segments[1];
+
+                        if (template_directory_name.Equals(segments[1]) ||
+                             segments[1].Equals(this.defaultTemplateName))
+                        {
+                            template_file_name = template_directory_name;
+                        }
+
+                    }
+                    else
+                    {
+                        template_directory_name = template_file_name.Substring(
+                            0, template_file_name
+                           .LastIndexOf(this.resourceNameSeparator + segments[segments.Length - 1]));
+
+                        var template_directory_name_temp = segments[segments.Length - 2];
+                        template_file_name_temp = segments[segments.Length - 1];
+
+                        if (template_directory_name_temp.Equals(template_file_name_temp) ||
+                             template_file_name_temp.Equals(this.defaultTemplateName))
+                        {
+                            template_file_name = template_file_name_temp;
+                        }
+                        else
+                        {
+                            template_file_name = template_directory_name_temp +
+                                                 this.resourceNameSeparator +
+                                                 template_file_name_temp;
+                        }
+                    }
+                }
+            }
+
+            var path = context.HttpContext.Server.MapPath(virtual_root +
+                                                          this.resourceNameSeparator +
+                                                          virtual_file_path);
+
+            template =  File.ReadAllText(path);
+            templateName = template_file_name.Replace(
+               this.fileNameSeparator,
+               this.resourceNameSeparator
+           );
+            return Tuple.Create(templateName, template);
+        }
+
+        public string defaultTemplateName = "default";
+        public string resourceNameSeparator = "/";
+        public string fileNameSeparator = "-";
+        public bool minifyTemplates = true;
+        public string jsPath { get; set; }
+    }
+}
